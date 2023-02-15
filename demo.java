71f03bf487d24fe9158f85de4d7cdb428f9b437c
rajitha <rajitha2125@gmail.com>
2023-02-15 11:39:47 -0500

    third commit

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class demo {
   public static void main(String[] args){
     System.out.println("Hello this is new file");
     System.out.println ("\n \n Hii this is second commit");
+    System.out.println(" Hi!!!!!!!!!!!!!!!!!");
   }
 }
